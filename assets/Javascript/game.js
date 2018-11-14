432101ebda24b8cd80c5f30190e172f56e94b090
Dina <counting1121@yahoo.com>
2018-11-13 19:05:22 -0500

    completed with cleaned up comments and no console logs

--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -1,22 +1,23 @@
 // Creates an array of all letter choices
-
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+//Create an empty array for the user choices
 var userChoices = [];
 
 
 // Create variables to hold stats
-
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
 
 //Create a function to take in user input
-
 document.onkeyup = function (event) {
+    
     //Holds key that was input
     var userGuess = event.key;
-    userChoices.push(userGuess);
 
+    //Saves user input and puts it into an array
+    userChoices.push(userGuess);
 
     //Computer randomly chooses a letter from the array
     var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
@@ -25,14 +26,18 @@ document.onkeyup = function (event) {
     //Write logic 
     
         if ((userGuess === computerGuess)) {
+            //If user wins, user count goes up, guessesLeft resets to 9 and the array of user inputs is emptied
             wins++;
             guessesLeft = 9;
             userChoices.length = 0;
             }      
          else if ((userGuess !== computerGuess)) {
-        guessesLeft--;
-    
+        //If guess is wrong guesses left goes down
+            guessesLeft--;
+            
+            //Checks to see is max # of guesses has been used
         if (guessesLeft === 0) {
+            //If there are no more guesses left, the guesses left are reset to 9, losses go up and the array of user inputs is emptied
             guessesLeft = 9;
             losses++;
             userChoices.length = 0;
@@ -40,16 +45,6 @@ document.onkeyup = function (event) {
         }
     }
 
-    
-
-
-    console.log(userGuess);
-    console.log(computerGuess);
-    console.log(wins);
-    console.log(losses);
-    console.log(guessesLeft);
-    console.log(userChoices);
-    console.log(userChoices.length);
 
 
     //Show results on screen
